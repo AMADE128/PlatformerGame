19e762f211767a795b1e80b0a9ff6b5ebfbb8555
Marc Pavon Llop <60131792+Marckitus@users.noreply.github.com>
2020-10-28 16:54:32 +0100

    Added player.h and player.cpp

---
+++ Platformer/Game/Source/Player.cpp
@@ -0,0 +1,33 @@
+#include "Player.h"
+#include "App.h"
+#include "Textures.h"
+#include "Log.h"
+
+
+Player::Player() : Module()
+{
+	name.Create("map");
+}
+
+// Destructor
+Player::~Player()
+{}
+
+bool Player::Awake()
+{
+	LOG("Init Player");
+	bool ret = true;
+
+	return ret;
+}
+
+bool Player::Start()
+{
+	LOG("Loading player textures");
+	
+	//Cargar texturas
+	
+	//Posicion x e y
+
+	return true;
+}
---
+++ Platformer/Game/Source/Player.h
@@ -0,0 +1,30 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "Module.h"
+
+class Player : public Module
+{
+
+public:
+
+	Player();
+
+	// Destructor
+	virtual ~Player();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called each loop iteration
+	bool PreUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+};
+
+#endif // __PLAYER_H__
