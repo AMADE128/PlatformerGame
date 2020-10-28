fedd9c5d8e77d45cdfce1428996550c700599fba
Marc Pavon Llop <60131792+Marckitus@users.noreply.github.com>
2020-10-28 19:07:28 +0100

    Drawing player
    missing paths

--- Platformer/Game/Source/Player.cpp
+++ Platformer/Game/Source/Player.cpp
@@ -1,33 +1,57 @@
-//#include "Player.h"
-//#include "App.h"
-//#include "Textures.h"
-//#include "Log.h"
-//
-//
-//Player::Player() : Module()
-//{
-//	name.Create("map");
-//}
-//
-//// Destructor
-//Player::~Player()
-//{}
-//
-//bool Player::Awake()
-//{
-//	LOG("Init Player");
-//	bool ret = true;
-//
-//	return ret;
-//}
-//
-//bool Player::Start()
-//{
-//	LOG("Loading player textures");
-//	
-//	//Cargar texturas
-//	
-//	//Posicion x e y
-//
-//	return true;
-//}
+#include "Player.h"
+#include "App.h"
+#include "Textures.h"
+#include "Log.h"
+
+
+Player::Player() : Module()
+{
+	name.Create("map");
+}
+
+// Destructor
+Player::~Player()
+{}
+
+bool Player::Awake()
+{
+	LOG("Init Player");
+	bool ret = true;
+
+	return ret;
+}
+
+bool Player::Start()
+{
+	LOG("Loading player textures");
+	
+	//Cargar texturas
+	Idle = app->tex->Load("Output/Assets/textures/Character/Idle (32x32).png");
+	Fall = app->tex->Load("Output/Assets/textures/Character/Fall.png");
+	Hit = app->tex->Load("Output/Assets/textures/Character/Hit (32x32).png");
+	Run = app->tex->Load("Output/Assets/textures/Character/Run (32x32).png");
+	Jump = app->tex->Load("Output/Assets/textures/Character/Jump (32x32).png");
+	//Posicion x e y
+
+	return true;
+}
+
+bool Player::PostUpdate()
+{
+
+	return true;
+}
+
+bool Player::CleanUp()
+{
+	LOG("Unloading particles");
+
+	//Aqui deletamos cosas
+	app->tex->UnLoad(Idle);
+	app->tex->UnLoad(Fall);
+	app->tex->UnLoad(Hit);
+	app->tex->UnLoad(Run);
+	app->tex->UnLoad(Jump);
+
+	return true;
+}
--- Platformer/Game/Source/Player.h
+++ Platformer/Game/Source/Player.h
@@ -1,30 +1,35 @@
-//#ifndef __PLAYER_H__
-//#define __PLAYER_H__
-//
-//#include "Module.h"
-//
-//class Player : public Module
-//{
-//
-//public:
-//
-//	Player();
-//
-//	// Destructor
-//	virtual ~Player();
-//
-//	// Called before render is available
-//	bool Awake();
-//
-//	// Called before the first frame
-//	bool Start();
-//
-//	// Called each loop iteration
-//	bool PreUpdate();
-//
-//	// Called before quitting
-//	bool CleanUp();
-//
-//};
-//
-//#endif // __PLAYER_H__
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "Module.h"
+
+class Player : public Module
+{
+
+public:
+
+	Player();
+
+	// Destructor
+	virtual ~Player();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+	SDL_Texture* Idle = nullptr;
+	SDL_Texture* Fall = nullptr;
+	SDL_Texture* Hit = nullptr;
+	SDL_Texture* Run = nullptr;
+	SDL_Texture* Jump = nullptr;
+
+};
+
+#endif // __PLAYER_H__
