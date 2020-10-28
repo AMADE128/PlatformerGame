434206137535826fe42d1cc60c1f8ea7d343800a
himar33 <himar33@users.noreply.github.com>
2020-10-28 14:25:21 +0100

    Camera Lock

--- Platformer/Game/Source/Scene.cpp
+++ Platformer/Game/Source/Scene.cpp
@@ -58,16 +58,28 @@ bool Scene::Update(float dt)
 		app->SaveGameRequest();
 
 	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		app->render->camera.y -= 1;
+		if (app->render->camera.y > -880)
+		{
+			app->render->camera.y -= 1;
+		}
 
 	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		app->render->camera.y += 1;
+		if (app->render->camera.y < 0)
+		{
+			app->render->camera.y += 1;
+		}
 
 	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		app->render->camera.x -= 1;
+		if (app->render->camera.x > -1920)
+		{
+			app->render->camera.x -= 1;
+		}
 
 	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		app->render->camera.x += 1;
+		if (app->render->camera.x < 0)
+		{
+			app->render->camera.x += 1;
+		}
 
 	//app->render->DrawTexture(img, 380, 100); // Placeholder not needed any more
 
