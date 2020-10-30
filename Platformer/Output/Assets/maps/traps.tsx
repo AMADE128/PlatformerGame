5df6af41da0b18eca607bc99952fa0d3b6c53e17
himar33 <himar33@users.noreply.github.com>
2020-10-30 13:31:51 +0100

    Revert "Merge branch 'master' of https://github.com/AMADE128/PlatformerGame"
    This reverts commit a72b4d1155b02673072bfb2d987e96f0615d7dad, reversing
    changes made to 948e50c038d25a97f6af100b42d2d7876dca8be2.

--- Platformer/Output/Assets/maps/traps.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="traps" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="../../../../../Aprending_Team-Platformer_Alpha/Platformer/Output/Assets/maps/traps.png" width="48" height="48"/>
-</tileset>
