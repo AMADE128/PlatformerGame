43f6fea4979811d1e70e61c4a579e73cd50e6380
Pol Vazquez Porcar <60131834+AMADE128@users.noreply.github.com>
2020-11-30 20:22:36 +0100

    Level 2 colliders + Level 1 Cleaned

--- Platformer/Output/Assets/maps/traps.tsx
+++ Platformer/Output/Assets/maps/traps.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="traps" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="../../../../../Aprending_Team-Platformer_Alpha/Platformer/Output/Assets/maps/Idle.png" width="16" height="16"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Idle" tilewidth="48" tileheight="48" tilecount="1" columns="1">
+ <image source="Idle.png" width="48" height="48"/>
 </tileset>
